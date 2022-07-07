51ad716cb7afedd88f9129ce33e3cc8ea81e2fc4
andys8 <andys8@users.noreply.github.com>
2022-07-07 16:51:17 +0200

    Try out purescript-react-dom fork (createRoot)

--- packages.dhall
+++ packages.dhall
@@ -2,4 +2,27 @@ let upstream =
       https://github.com/purescript/package-sets/releases/download/psc-0.15.2-20220624/packages.dhall
         sha256:08989ed9f53e381f879f1b7012ad7684b1ed64d7164c4ad75e306d3210a46c92
 
-in  upstream
+let additions = {=}
+
+let overrides =
+      { react-basic-dom =
+        { dependencies =
+          [ "effect"
+          , "foldable-traversable"
+          , "foreign-object"
+          , "maybe"
+          , "nullable"
+          , "prelude"
+          , "react-basic"
+          , "unsafe-coerce"
+          , "web-dom"
+          , "web-events"
+          , "web-file"
+          , "web-html"
+          ]
+        , repo = "https://github.com/andys8/purescript-react-basic-dom.git"
+        , version = "5031446d1613a50474306c4c5435613cd4e04516"
+        }
+      }
+
+in  upstream // overrides // additions
--- src/Main.purs
+++ src/Main.purs
@@ -4,21 +4,18 @@ import Prelude
 
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Aff.Compat (EffectFn1, runEffectFn1)
 import Effect.Exception (throw)
-import Foreign.Daisyui (button, footer, footerTitle, hero, heroContent, navbar, navbarCenter, navbarEnd, navbarStart)
+import Foreign.Daisyui (button, footer, footerTitle, hero, heroContent, navbar, navbarStart)
 import Foreign.Object as O
 import React.Basic (JSX, element)
+import React.Basic.DOM.Client (createRoot, renderRoot)
 import React.Basic.DOM as R
 import React.Basic.Events (handler_)
 import React.Basic.Hooks (Component, component)
-import Web.DOM.Element (Element)
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
 import Web.HTML.Window (document)
-
-foreign import createRoot :: EffectFn1 Element { render :: EffectFn1 JSX Unit }
 
 main :: Effect Unit
 main = do
@@ -28,8 +25,8 @@ main = do
     Nothing -> throw "Could not find container element"
     Just container -> do
       app <- mkApp
-      { render } <- runEffectFn1 createRoot container
-      runEffectFn1 render (app {})
+      reactRoot <- createRoot container
+      renderRoot reactRoot (app {})
 
 mkApp :: Component {}
 mkApp = component "App" \_ -> React.do
