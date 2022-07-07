12e882a629121b773474f506ca7075d7a70c364f
andys8 <andys8@users.noreply.github.com>
2022-07-07 20:21:42 +0200

    Hot module reloading working without accept

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import { main } from "../output/Main/index";
-import file from "url:../data/prelude.txt";
-
-if (module.hot) {
-  module.hot.accept(function () {
-    console.log("Reloaded, running main again");
-    main();
-  });
-}
-
-console.log(file);
 
 main();
