0ed524cf95519aa0b73bfd08c858d47b59921d08
andys8 <andys8@users.noreply.github.com>
2022-07-14 01:04:22 +0200

    Include fira code font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@
 // TODO: Reduce tailwind css bundle size
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx,purs}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+    fontFamily: {
+      mono: ["Fira Code", "monospace"],
+    },
+  },
   safelist: [{ pattern: /./ }],
   plugins: [require("daisyui")],
   daisyui: {
