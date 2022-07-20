8e687a86cfb146c191e5a407412547a423577e20
andys8 <andys8@users.noreply.github.com>
2022-07-20 14:16:36 +0200

    Define tailwind content, css purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx,purs}"],
+  content: ["./src/**/*.{html,js,purs}", "node_modules/daisyui/dist/**/*.js"],
   theme: {
     extend: {},
     fontFamily: {
       mono: ["Fira Code", "monospace"],
     },
   },
-  safelist: [{ pattern: /./ }],
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["dracula"],
